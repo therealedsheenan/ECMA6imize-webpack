dbeabc4ad6220c9e5394838a40f891c3c411a603
Sheenan Tenepre <sheenan.tenepre@shiftbrain.co.jp>
2016-04-15 19:31:55 +0900

    update gulp tasks

---
+++ gulp/tasks/main/development.babel.js
@@ -0,0 +1,13 @@
+import gulp from 'gulp'
+import rs from 'run-sequence'
+
+let runDefault = ( cb ) => {
+    // rs(
+    //     ['clean', 'svgstore', 'modernizr'],
+    //     ['copy-imagemin', 'copy-fonts', 'html', 'styles', 'watchify'],
+    //     ['browserSync', 'watch', 'credits'],
+    //     cb
+    // )
+}
+
+gulp.task('development', runDefault)
